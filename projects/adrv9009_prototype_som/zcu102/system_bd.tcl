17a63d5e5bb12028b5b7be4e584b07d73ed5ab65
Adrian Costina <adrian.costina@analog.com>
2018-09-07 13:55:53 +0100

    adrv9009_prototype_som: set util_adxcvr parameters

--- projects/adrv9009_prototype_som/zcu102/system_bd.tcl
+++ projects/adrv9009_prototype_som/zcu102/system_bd.tcl
@@ -6,7 +6,6 @@ set dac_dma_data_width 256
 
 source $ad_hdl_dir/projects/common/zcu102/zcu102_system_bd.tcl
 source $ad_hdl_dir/projects/common/xilinx/dacfifo_bd.tcl
-#source $ad_hdl_dir/projects/common/zcu102/zcu102_plddr4_dacfifo_bd.tcl
 
 source ../common/adrv9009_p_som_bd.tcl
 
@@ -36,6 +35,31 @@ ad_ip_parameter axi_adrv9009_rx_os_xcvr CONFIG.XCVR_TYPE 2
 ad_ip_parameter util_adrv9009_p_som_xcvr CONFIG.XCVR_TYPE 2
 ad_ip_parameter util_adrv9009_p_som_xcvr CONFIG.QPLL_FBDIV 80
 ad_ip_parameter util_adrv9009_p_som_xcvr CONFIG.QPLL_REFCLK_DIV 1
+
+ad_ip_parameter util_adrv9009_p_som_xcvr CH_HSPMUX  0x2424        ;#16'b0010010000100100
+ad_ip_parameter util_adrv9009_p_som_xcvr CPLL_CFG0 0x03fe         ;#16'b0000001111111110
+ad_ip_parameter util_adrv9009_p_som_xcvr CPLL_CFG1 0x021          ;#16'b0000000000100001
+ad_ip_parameter util_adrv9009_p_som_xcvr CPLL_CFG2 0x0203         ;#16'b0000001000000011
+ad_ip_parameter util_adrv9009_p_som_xcvr PREIQ_FREQ_BST 0
+ad_ip_parameter util_adrv9009_p_som_xcvr PCIE_BUFG_DIV_CTRL 0x3500;#16'b0011010100000000
+ad_ip_parameter util_adrv9009_p_som_xcvr PCIE_PLL_SEL_MODE_GEN12 2
+ad_ip_parameter util_adrv9009_p_som_xcvr RXCDR_CFG3 0x0012        ;#16'b0000000000010010
+ad_ip_parameter util_adrv9009_p_som_xcvr RXCDR_CFG3_GEN2 0x12     ;#6'b010010
+ad_ip_parameter util_adrv9009_p_som_xcvr RXCDR_CFG3_GEN3 0x0012   ;#16'b0000000000010010
+ad_ip_parameter util_adrv9009_p_som_xcvr RXPI_CFG0 0x0102         ;#16'b0000000100000010
+ad_ip_parameter util_adrv9009_p_som_xcvr RXPI_CFG1 0x0015         ;#16'b0000000000010101
+ad_ip_parameter util_adrv9009_p_som_xcvr RX_WIDEMODE_CDR 0
+ad_ip_parameter util_adrv9009_p_som_xcvr TXPH_CFG 0x0323          ;#16'b0000001100100011
+ad_ip_parameter util_adrv9009_p_som_xcvr TXPI_CFG 0x0054          ;#16'b0000000001010100
+ad_ip_parameter util_adrv9009_p_som_xcvr TXPI_CFG3 0
+ad_ip_parameter util_adrv9009_p_som_xcvr TXPI_CFG4 1
+ad_ip_parameter util_adrv9009_p_som_xcvr TX_PI_BIASSET 1
+
+ad_ip_parameter util_adrv9009_p_som_xcvr PPF0_CFG 0x0600      ;#16'b0000011000000000
+ad_ip_parameter util_adrv9009_p_som_xcvr QPLL0_CFG2 0x0fc1    ;#16'b0000111111000001
+ad_ip_parameter util_adrv9009_p_som_xcvr QPLL0_CFG2_G3 0x0fc1 ;#16'b0000111111000001
+ad_ip_parameter util_adrv9009_p_som_xcvr QPLL0_CFG4 0x0003    ;#16'b0000000000000011
+ad_ip_parameter util_adrv9009_p_som_xcvr QPLL0_LPF 0x37f      ;#10'b1101111111
 
 ad_ip_parameter axi_adrv9009_rx_clkgen CONFIG.DEVICE_TYPE 2
 ad_ip_parameter axi_adrv9009_tx_clkgen CONFIG.DEVICE_TYPE 2
